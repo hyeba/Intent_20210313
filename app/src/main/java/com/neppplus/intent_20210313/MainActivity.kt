326c9f8a124055d4c09f5041415fee529cdab162
장혜진 <aslk3366zz@naver.com>
2021-03-13 14:40:25 +0900

    DIAL 기능 활용 예제

--- app/src/main/java/com/neppplus/intent_20210313/MainActivity.kt
+++ app/src/main/java/com/neppplus/intent_20210313/MainActivity.kt
@@ -2,6 +2,7 @@ package com.neppplus.intent_20210313
 
 import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
@@ -52,6 +53,25 @@ class MainActivity : AppCompatActivity() {
             startActivityForResult(myIntent, REQ_FOR_NICKNAME)
 
         }
+
+        dialBtn.setOnClickListener {
+
+//            입력한 폰번이 뭔지?
+            val inputPhoneNum = phoneNumEdt.text.toString()
+
+//            Uri - 어디로 전화를 걸건지 등 세부 정보 표현
+            val myUri = Uri.parse("tel:${inputPhoneNum}")
+
+//            Intent 생성 = 출발지/도착지 대신, 안드로이드의 어떤 기능(action)? + 세부 정보(uri)?
+            val myIntent = Intent(Intent.ACTION_DIAL, myUri)
+
+//            실제 화면 이동
+            startActivity(myIntent)
+
+        }
+
+
+
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
